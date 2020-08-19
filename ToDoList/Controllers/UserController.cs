13dcfa972364b65a696687fce807c904a5f6f15b
Daniel Johansson <johansson.gd@gmail.com>
2020-08-19 20:24:50 +0200

    lagt till GetUser till UserController

--- ToDoList/Controllers/UserController.cs
+++ ToDoList/Controllers/UserController.cs
@@ -3,14 +3,18 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using AutoMapper;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using ToDoList.Data;
+using ToDoList.Dtos;
+using ToDoList.Models;
 
 namespace ToDoList.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
+    [AllowAnonymous] // TODO - Ta bort när lagt till Jwt token
     public class UserController : ControllerBase
     {
         private readonly IToDoListRepository _repo;
@@ -22,9 +26,25 @@ namespace ToDoList.Controllers
             _mapper = mapper;
         }
 
-        //public async Task<IActionResult> GetUsers()
-        //{
-        //    var users = _repo
-        //}
+        [HttpGet]
+        public async Task<IActionResult> GetUsers()
+        {
+            var users = await _repo.GetUsers();
+
+            var usersToReturn = _mapper.Map<IEnumerable<UserToReturnDto>>(users);
+
+            return Ok(usersToReturn);
+
+        }
+
+        [HttpGet("{id}", Name = "GetUser")]
+        public async Task<IActionResult> GetUser(int id)
+        {
+            var user = await _repo.GetUser(id);
+
+            var userToReturn = _mapper.Map<UserToReturnDto>(user);
+
+            return Ok(userToReturn);
+        }
     }
 }
