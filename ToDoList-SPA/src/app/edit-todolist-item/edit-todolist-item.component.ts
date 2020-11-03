c1d662fddf841e5c5d8486803691b669836a3ec9
Daniel Johansson <johansson.gd@gmail.com>
2020-11-03 19:04:36 +0100

    Fixat så att det finns en form för edit to-do-list item som fungerar och fylls med data från servern

--- ToDoList-SPA/src/app/edit-todolist-item/edit-todolist-item.component.ts
+++ ToDoList-SPA/src/app/edit-todolist-item/edit-todolist-item.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { ListItem } from '../_models/list-item';
 import { ActivatedRoute } from '@angular/router';
+import { FormGroup, FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-edit-todolist-item',
@@ -10,13 +11,31 @@ import { ActivatedRoute } from '@angular/router';
 export class EditTodolistItemComponent implements OnInit {
 
   itemForEdit: ListItem;
+  editItemForm: FormGroup;
 
-  constructor(private route: ActivatedRoute) { }
+
+  constructor(private route: ActivatedRoute, private fb: FormBuilder) { }
 
   ngOnInit(): void {
     this.route.data.subscribe(data => {
       this.itemForEdit = data['itemForEdit'];
     });
+
+    this.createEditItemForm();
+  }
+
+  createEditItemForm() {
+    let dateCompleteBy = new Date(this.itemForEdit.completeBy);
+
+    this.editItemForm = this.fb.group({
+      itemText: [this.itemForEdit.itemText],
+      completeBy: [dateCompleteBy],
+      completed: [this.itemForEdit.completed]
+    });
+  }
+
+  updateItem() { // TODO - Fixa den här funktionen (obs kolla så att API-updaten funkar). Snygga till html. Completed fungerar inte, Invalid date. Har med ngx-bootsrap att göra.
+
   }
 
 }
